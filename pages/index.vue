65823fe1b3a728e8bc1a5d1358a5827f1f429a4b
AbelMaireg <abelrighthere@gmail.com>
2025-04-02 16:54:52 +0300

    INITIAL COMMIT
        - prettier
        - tailwindcss
        - shadcn

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <Button
+    class="bg-red-500 text-white px-4 py-2 rounded"
+    @click="() => $nuxt.$router.push('/about')"
+    >Hello</Button
+  >
+</template>
