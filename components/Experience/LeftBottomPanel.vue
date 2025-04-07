0dcd306a5d1ce3d137bd0960d936bfa08a761006
AbelMaireg <abelrighthere@gmail.com>
2025-04-07 18:30:46 +0300

    feat: source code badge enabled

--- components/Experience/LeftBottomPanel.vue
+++ components/Experience/LeftBottomPanel.vue
@@ -7,7 +7,13 @@ defineProps<{
 
 <template>
   <div class="flex flex-row gap-2">
-    <Badge v-if="goto"><a href="https://mamitoapp.com">Open</a></Badge>
-    <Badge>{{ sourceCode || "Closed Source" }}</Badge>
+    <Badge v-if="goto"><a :href="goto">Open</a></Badge>
+
+    <Badge v-if="sourceCode"
+      ><a href="https://github.com/AbelMaireg/automated-traffic-flow-management"
+        >Goto</a
+      ></Badge
+    >
+    <Badge v-else>Closed</Badge>
   </div>
 </template>
